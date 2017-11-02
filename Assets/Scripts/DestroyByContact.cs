97bff13ee04853a8361344d071cfd61aeb2503cd
Sam Kumer <sam.kumer@r132-32-w10.SCHULE-ISERV>
2017-11-02 14:00:20 +0100

    first commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		while (other.tag == "Boundary") 
+		{
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
